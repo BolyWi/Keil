e93660541ff73023583eae74cf844f499dd3186d
走在路上cool <zzls-cool@foxmail.com>
2024-07-28 23:42:37 +0800

    串口模块优化

--- src/uart.c
+++ src/uart.c
@@ -2,6 +2,9 @@
 #include "uart.h"
 
 #if ENABLE_UART_MOUDLE
+// uart receive buf
+static char recv_buf[128] = { 0 };
+
 void uart_config()
 {
     SCON = 0x50;
@@ -14,6 +17,7 @@ void uart_config()
     TI = 0;
     RI = 0;
     ES = 1;
+    EA = 1;
 }
 
 void uart_send(unsigned char buf)
@@ -31,5 +35,34 @@ char putchar(char c)
     TI = 0;
     ES = 1;
     return c;
+}
+
+void uart_recv() interrupt 4
+{
+    int len = 0;
+    int total_len = sizeof(recv_buf);
+    len = strlen(recv_buf) % total_len;
+    if(RI == 1)
+    {
+        if(len == 0)
+            memset(recv_buf, 0, total_len);
+        recv_buf[len] = SBUF;
+        len++;
+        RI = 0;
+    }
+}
+
+int get_uart_info(char* buf, int buf_len)
+{
+    int len = strlen(recv_buf);
+    if(recv_buf !=NULL && strlen(recv_buf) > 0)
+    {
+        if(buf_len <= len)
+            len = buf_len;
+        strncpy(buf, recv_buf, len);
+        memset(recv_buf, 0, sizeof(recv_buf));
+        return OK;
+    }
+    return ERROR;
 }
 #endif
--- src/uart.h
+++ src/uart.h
@@ -1,5 +1,7 @@
 #ifndef __UART_H__
 #define __UART_H__
+#include <string.h>
+#include <stdlib.h>
 
 // 初始化串口
 void uart_config();
@@ -8,4 +10,5 @@ void uart_send(unsigned char buf);
 /*重定向putchar() 用于串口log输出*/
 char putchar(char c);
 
+int get_uart_info(char* buf, int buf_len);
 #endif // __UART_H__
